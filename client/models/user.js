fe865a2678e4a4f21c72b69888ae206b226ed893
Jon Edwards <jkedwards@me.com>
2015-05-22 15:14:26 -0700

    create user, list user, edit user, and delete user all in place

--- client/models/user.js
+++ client/models/user.js
@@ -4,10 +4,29 @@ angular.module('wisely')
 .factory('User', function($rootScope, $http, nodeUrl){
   function User(){}
 
-  User.login = function(user){
-    $http.post(nodeUrl + '/users/authenticate', user);
+  User.create = function(user){
+    return $http.post(nodeUrl + '/users', user);
   };
 
+  User.login = function(user){
+    return $http.post(nodeUrl + '/users/authenticate', user);
+  };
+
+  User.index = function(){
+    return $http.get(nodeUrl + '/users');
+  };
+
+  User.remove = function(user){
+    return $http.delete(nodeUrl + '/users/' + user._id + '/delete');
+  };
+
+  User.save = function(user){
+    var o = user;
+    delete o.__v;
+    delete o.createdAt;
+    delete o._id;
+    return $http.put(nodeUrl + '/users/' + user._id + '/update', o);
+  };
 
   return User;
 });
---
+++ client/views/users/admin.js
@@ -0,0 +1,63 @@
+'use strict';
+
+angular.module('wisely')
+.controller('UserAdminCtrl', function($scope, User, $window){
+  $scope.createOrEdit = false;
+  function getUsers(){
+    User.index()
+    .then(function(response){
+      $scope.users = response.data;
+    });
+  }
+  getUsers();
+
+  $scope.edit = function(user){
+    $scope.editUser = $scope.createOrEdit = true;
+    $scope.user = user;
+  };
+
+  $scope.save = function(user){
+    User.save(user)
+    .then(function(response){
+      $window._.update($scope.users, function(userFromArray){
+        return userFromArray._id === response.data._id;
+      });
+      $scope.editUser = $scope.createOrEdit = false;
+    })
+    .catch(function(response){
+      console.log(response);
+      $window.swal({title: 'User Save Error', text: 'There was an error saving the user changes.', type: 'error'});
+    });
+  };
+
+  $scope.remove = function(user){
+    User.remove(user)
+    .then(function(response){
+      $window._.remove($scope.users, function(userFromArray){
+        return userFromArray._id === response.data._id;
+      });
+    });
+  };
+
+  $scope.create = function(user){
+    if($scope.password1 === $scope.password2){
+      user.password = $scope.password1;
+      User.create(user)
+      .then(function(response){
+        $window._.update($scope.users, function(userFromArray){
+          return userFromArray._id === response.data._id;
+        });
+        $scope.createOrEdit = false;
+      })
+      .catch(function(response){
+        console.error(response);
+      });
+    }else{
+      $window.swal({title: 'Password Error', text: 'The passwords must match.', type: 'error'});
+    }
+  };
+
+  $scope.createButton = function(){
+    $scope.createOrEdit = true;
+  };
+});
