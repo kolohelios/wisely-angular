d91d8692611251341f42964f05f8e23f80c75353
Jon Edwards <jkedwards@me.com>
2015-05-25 10:02:59 -0700

    continue dev on project collections, ui and ux enhancements

--- client/views/projects/projects-list.js
+++ client/views/projects/projects-list.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('wisely')
-.controller('ProjectsListCtrl', function(Project, $scope, $state){
+.controller('ProjectsListCtrl', function(Project, $scope, $state, $window){
   Project.index()
   .then(function(response){
     $scope.projects = response.data;
@@ -13,6 +13,15 @@ angular.module('wisely')
 
   $scope.edit = function(projectId){
     $state.go('projects.edit', {projectId: projectId});
+  };
+
+  $scope.remove = function(projectId){
+    Project.remove(projectId)
+    .then(function(response){
+      $window._.remove($scope.projects, function(projectFromArray){
+        return projectFromArray._id === response.data._id;
+      });
+    });
   };
 
   $scope.showProject = function(projectId){
