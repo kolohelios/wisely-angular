b2c5ae2379df25a8d20064a8029938650600377e
Jon Edwards <jkedwards@me.com>
2015-05-25 19:59:43 -0700

    more progress with project show and general ui-ux improvements

--- client/views/users/login.js
+++ client/views/users/login.js
@@ -1,13 +1,21 @@
 'use strict';
 
 angular.module('wisely')
-.controller('UserLoginCtrl', function($scope, $rootScope, $state, $window, User, $http){
+.controller('UserLoginCtrl', function($scope, $rootScope, $state, $window, User, $http, Project){
   $scope.login = function(user){
     User.login(user)
     .then(function(response){
       $window.localStorage.wiselyJWTToken = response.data.token;
       $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
       $rootScope.activeUser = response.data.user;
+      Project.index()
+      .then(function(projects){
+        if(projects.data.length === 1 && $rootScope.activeUser.role === 1){
+          $state.go('projects.show', {projectId: projects.data[0]._id});
+        }else{
+          $state.go('projects.list');
+        }
+      });
       $state.go('home');
     })
     .catch(function(){
